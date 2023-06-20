353002ff4d66acd8a382866fbb6140fd4aca3f03
Msambi-ops <msambiroman@gmail.com>
2023-06-20 11:12:42 +0300

    function

---
+++ test/function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//function declaration
+int max(int num1, int num2);
+
+int main()
+{
+//define local variables
+	int a = 100;
+	int b = 200;
+	int ret;
+
+//call the function to get maximum value
+	ret = max(a, b);
+	printf("The max value of a and b is %d\n", ret);
+
+	return 0;
+}
+
+// function returning the maximum between the two numbers
+int max(int num1, int num2)
+{
+//declare a local variable - computation
+	int result;
+	if (num1 > num2)
+	result = num1;
+	else
+	result = num2;
+	return result;
+}
